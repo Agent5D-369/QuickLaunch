9eed37278c61a1d917dedcfc67b92f2a015da335
Rick Broider <rbroider@gmail.com>
2026-06-04 17:11:14 -0600

    Enable GA4 measurement ID

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,6 +1,6 @@
 window.QUICKLAUNCH_CONFIG = {
   // Add your GA4 Measurement ID, for example: "G-XXXXXXXXXX"
-  ga4MeasurementId: '',
+  ga4MeasurementId: 'G-NPCCGNX5FQ',
 
   // Add your Microsoft Clarity project ID, for example: "abcd1234ef"
   clarityProjectId: ''
